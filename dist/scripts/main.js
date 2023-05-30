ddc8a633e94a64c97d1d0d8f07d6ff78883813a1
m.alekseev <alxvm1@mail.ru>
2023-05-31 03:38:46 +0700

    refactor: завершена верстка главной страницы (без скриптов и адаптива)

---
+++ dist/scripts/main.js
@@ -0,0 +1,34 @@
+(function () {
+   "use strict";
+
+   const eventPP = document.querySelector("#js-eventPP");
+   if (eventPP) {
+      const eventOpenBtn = document.querySelector("#js-eventOpenBtn");
+      const closeEventPP = function (event) {
+         function close() {
+            document.removeEventListener("keyup", closeEventPP);
+            eventPP.removeEventListener("click", closeEventPP);
+            root.classList.remove("show-event-popup");
+         }
+         switch (event.type) {
+            case "keyup":
+               if (event.key === "Escape" || event.keyCode === 27) close();
+               break;
+            case "click":
+               if (
+                  event.target === this ||
+                  event.target.classList.contains("js-ppCloseBtn")
+               )
+                  close();
+               break;
+         }
+      };
+      eventOpenBtn.addEventListener("click", function () {
+         root.classList.add("show-event-popup");
+         document.addEventListener("keyup", closeEventPP);
+         eventPP.addEventListener("click", closeEventPP);
+      });
+   }
+
+})();
+
